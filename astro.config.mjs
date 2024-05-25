7a6f0b51a3d20da00395455a057c3d7954057c07
Aarin Dave <aarindave@gmail.com>
2024-05-25 13:21:09 -0400

    Fixed deployment error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gamerlots.github.io',
+  site: 'https://aarindave.github.io',
   integrations: [tailwind()]
 });
